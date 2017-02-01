a6af2840838780680af91680858ceb18c1646325
Vasyukov Yevgeniy <e.vasyukov@solarsecurity.ru>
2017-02-01 20:58:52 +0300

    debug

--- src/tsung/ts_client.erl
+++ src/tsung/ts_client.erl
@@ -1177,7 +1177,8 @@ set_thinktime(Think) ->
 
 
 start_tls(NS) ->
-  ?LOGF("$tarting TLS on socket = ~p . ~n", [NS#state_rcv.socket], ?INFO),
+  PORT1 = inet:getopts(NS#state_rcv.socket, [port]),
+  ?LOGF("$tarting TLS on socket = ~p , port = ~p. ~n", [NS#state_rcv.socket], PORT1, ?INFO),
   {ok, SSL} = ts_ssl:connect(NS#state_rcv.socket, []),
 
   NNS = NS#state_rcv{
